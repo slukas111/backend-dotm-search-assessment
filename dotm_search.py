f0f04e8c9421143ffb493bb54c48d4511c06c00e
slukas111 <sashamati@gmail.com>
2020-03-06 06:49:11 -0800

    importation test

--- dotm_search.py
+++ dotm_search.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#importation
+
+import sys
+import argparse
+import zipfile
 
 """
 Given a directory path, search all files in the path for a given text string
 within the 'word/document.xml' section of a MSWord .dotm file.
 """
-__author__ = "???"
+__author__ = "Sasha Lukas"
+
 
 
 def main():
